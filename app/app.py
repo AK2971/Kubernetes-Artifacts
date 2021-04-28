a1815b3f7eced910b3a6720c688c995c761ccd27
AK2971 <80771626+AK2971@users.noreply.github.com>
2021-04-28 19:12:05 +0530

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+import json
+
+app = Flask(__name__)
+
+@app.route('/pucsd')
+def myapi():
+
+    hostname=socket.gethostname()
+    ip=socket.gethostbyname(hostname) 
+    now = datetime.now()
+        
+    data=[{
+    	"Current Time in IST" : now.strftime("%H:%M:%S"),
+	"IP Address" : ip,
+	"Hostname" : hostname,
+	"City" : "Hingoli",
+	"Region" : "Maharashtra",
+	"Country" : "India" 
+    }]
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
